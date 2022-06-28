d055cdb2a32ce491f218267ac68ef833c55ecad6
jorneylopez <jorney95@live.com>
2022-06-28 16:53:29 -0600

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { MessagesModule } from './messages/messages.module';
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+
+async function bootstrap() {
+  const app = await NestFactory.create(MessagesModule);
+  app.useGlobalPipes(
+    new ValidationPipe()
+  );
+  await app.listen(3000);
+}
+bootstrap();
---
+++ src/messages/messages.controller.ts
@@ -0,0 +1,19 @@
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+
+@Controller('messages')
+export class MessagesController {
+    @Get()
+    listMessages() {
+
+    }
+
+    @Post()
+    createMessages(@Body() body: any) {
+        console.log(body);
+    }
+
+    @Get('/:id')
+    getMessage(@Param('id') id: string) {
+        console.log(id);
+    }
+}
