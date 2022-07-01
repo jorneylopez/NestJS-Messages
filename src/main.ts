0ae83bc7c783e6ebabf9b91ffd93a4a508ac70f3
jorneylopez <jorney95@live.com>
2022-06-30 23:10:52 -0600

    structure DI

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
-import { MessagesModule } from './messages/messages.module';
+// import { MessagesModule } from './messages/messages.module';
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { ComputerModule } from './computer/computer.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(MessagesModule);
+  // const app = await NestFactory.create(MessagesModule);
+  const app = await NestFactory.create(ComputerModule);
   app.useGlobalPipes(
     new ValidationPipe()
   );
