10bfec980be9ce72fe50be2f3dfd335517079549
jorneylopez <jorney95@live.com>
2022-07-05 21:13:40 -0600

    final  commit multi app running

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,29 @@
-// import { MessagesModule } from './messages/messages.module';
+import { MessagesModule } from './messages/messages.module';
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { ComputerModule } from './computer/computer.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express'
+
+
 
 async function bootstrap() {
-  // const app = await NestFactory.create(MessagesModule);
-  const app = await NestFactory.create(ComputerModule);
-  app.useGlobalPipes(
+  const server = express()
+
+  const appComputerModule = await NestFactory.create(ComputerModule, new ExpressAdapter(server));
+  const appMessagesModule = await NestFactory.create(MessagesModule, new ExpressAdapter(server));
+
+  await appComputerModule.init();
+  await appMessagesModule.init();
+
+  appComputerModule.useGlobalPipes(
     new ValidationPipe()
   );
-  await app.listen(3000);
+
+  appMessagesModule.useGlobalPipes(
+    new ValidationPipe()
+  );
+
+  server.listen(3000);
 }
 bootstrap();
